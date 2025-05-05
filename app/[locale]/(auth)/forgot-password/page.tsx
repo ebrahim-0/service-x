45d68abfd827d2c0793a0de871b9645c645dd547
Ibrahim Abdelrazik <ebrahimabdelrazik2002@gmail.com>
2025-05-05 06:17:04 +0300

    feat(forgot-password): restructure ForgotPassword page layout to include logo above the form

--- app/[locale]/(auth)/forgot-password/page.tsx
+++ app/[locale]/(auth)/forgot-password/page.tsx
@@ -3,7 +3,10 @@ import { ForgotPasswordForm } from "@/components/auth/ForgotPasswordForm";
 const page = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <ForgotPasswordForm />
+      <div className="flex flex-col gap-7">
+        <img src="/assets/logo.svg" alt="Logo" className="w-full h-20 mb-2" />
+        <ForgotPasswordForm />
+      </div>
     </div>
   );
 };
